8fdd28012fe63e79adbda0393f75174df3397aaf
Gatlin C Johnson <gatlin@niltag.net>
2014-06-02 10:59:58 -0500

    docs

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 OPTS = -no-user-package-db -package-db .cabal-sandbox/*-packages.conf.d
 
-all: psilo
+all: doc psilo
+
+doc:
+	sh makedoc.sh
 
 preprocessor:
 	ghc $(OPTS) --make preprocessor.hs -o preprocessor
@@ -9,5 +12,6 @@ psilo:
 	ghc $(OPTS) --make src/{Evaluator,Syntax,Lexer,Parser,Main}.lhs -o psilo
 
 clean:
+	rm -r doc
 	rm psilo
 	rm src/*.{hi,o}
--- makedoc.sh
+++ makedoc.sh
@@ -6,14 +6,22 @@
 if [ ! -d doc ]; then
     mkdir doc
     mkdir -p doc/tex
+    mkdir -p doc/pdf
 #    mkdir -p doc/html
 fi
 
+
+###
+# readme
+pandoc -s -t latex -f markdown -o ./doc/tex/readme.tex
+
+###
+# src dir
 for f in $(ls src/*.lhs); do
     filename=`basename $f`
     extension="${filename##*.}"
     filename="${filename%.*}"
     lowername=$(echo $filename | tr '[:upper:]' '[:lower:]')
-
-    pandoc -f markdown+lhs -o ./doc/tex/$lowername.tex -i $f
+    out="$lowername.tex"
+    pandoc -s -t latex  -f markdown+lhs -o ./doc/tex/$out $f
 done
