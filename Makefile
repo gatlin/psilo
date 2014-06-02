46b28ba472583d83f994e79f7fa5aeec9bf2365e
Gatlin C Johnson <gatlin@niltag.net>
2014-06-02 15:55:30 -0500

    website generation

--- Makefile
+++ Makefile
@@ -2,9 +2,6 @@ OPTS = -no-user-package-db -package-db .cabal-sandbox/*-packages.conf.d
 
 all: doc psilo
 
-doc:
-	sh makedoc.sh
-
 preprocessor:
 	ghc $(OPTS) --make preprocessor.hs -o preprocessor
 
@@ -12,6 +9,5 @@ psilo:
 	ghc $(OPTS) --make src/{Evaluator,Syntax,Lexer,Parser,Main}.lhs -o psilo
 
 clean:
-	rm -r doc
 	rm psilo
 	rm src/*.{hi,o}
--- doc/makedoc.sh
+++ doc/makedoc.sh
@@ -3,29 +3,30 @@
 ###
 # Generate the documentation from sources
 
-if [ ! -d doc ]; then
-    mkdir doc
-    mkdir -p doc/tex
-    mkdir -p doc/pdf
-#    mkdir -p doc/html
-fi
+mkdir -p tex
+mkdir -p html
+mkdir -p pdf
+
+pdfoutputdir="./pdf"
 
 ###
 # if pandoc is present, we will generate various documentation formats
 if hash pandoc 2>/dev/null; then
     ###
     # readme
-    pandoc -s -t latex -f markdown -o ./doc/tex/readme.tex README.md
+    pandoc -s -t latex -f markdown -o ./tex/readme.tex ../README.md
 
     ###
     # src dir
-    for f in $(ls src/*.lhs); do
+    for f in $(ls ../src/*.lhs); do
         filename=`basename $f`
         extension="${filename##*.}"
         filename="${filename%.*}"
-        lowername=$(echo $filename | tr '[:upper:]' '[:lower:]')
-        out="$lowername.tex"
-        pandoc -s -t latex  -f markdown+lhs -o ./doc/tex/$out $f
+        out=$(echo $filename | tr '[:upper:]' '[:lower:]')
+        pandoc -s -t latex  -f markdown+lhs -o ./tex/$out.tex $f
+        pandoc -s -t html5  -f markdown+lhs \
+        --template=templates/src.tmpl.html \
+        -o ./html/$out.html $f
     done
 else
     echo "Install pandoc to generate documentation."
@@ -35,13 +36,13 @@ fi
 ###
 # if pdflatex is present, we will generate PDFs from the TeX sources.
 if hash pdflatex 2>/dev/null; then
-    for f in $(ls doc/tex/*.tex); do
-        filename=`basename $f`
 
-        pdflatex --output-directory=./doc/pdf ./doc/tex/$filename
+    for f in $(ls tex/*.tex); do
+        filename=`basename $f`
+        pdflatex --output-directory=$pdfoutputdir ./tex/$filename
     done
 
-    rm doc/pdf/*.{aux,log,out}
+    rm pdf/*.{aux,log,out}
 else
     echo "Install pdflatex to convert TeX documentation sources to PDF."
 fi
