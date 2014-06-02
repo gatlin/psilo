9f99c951286641f3e1d3ec1eee1945d597f05fab
Gatlin C Johnson <gatlin@niltag.net>
2014-06-02 15:20:28 -0500

    make sure not to use utilities th user does not have

--- makedoc.sh
+++ makedoc.sh
@@ -11,16 +11,37 @@ if [ ! -d doc ]; then
 fi
 
 ###
-# readme
-pandoc -s -t latex -f markdown -o ./doc/tex/readme.tex README.md
+# if pandoc is present, we will generate various documentation formats
+if hash pandoc 2>/dev/null; then
+    ###
+    # readme
+    pandoc -s -t latex -f markdown -o ./doc/tex/readme.tex README.md
+
+    ###
+    # src dir
+    for f in $(ls src/*.lhs); do
+        filename=`basename $f`
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        lowername=$(echo $filename | tr '[:upper:]' '[:lower:]')
+        out="$lowername.tex"
+        pandoc -s -t latex  -f markdown+lhs -o ./doc/tex/$out $f
+    done
+else
+    echo "Install pandoc to generate documentation."
+    exit 0
+fi
 
 ###
-# src dir
-for f in $(ls src/*.lhs); do
-    filename=`basename $f`
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    lowername=$(echo $filename | tr '[:upper:]' '[:lower:]')
-    out="$lowername.tex"
-    pandoc -s -t latex  -f markdown+lhs -o ./doc/tex/$out $f
-done
+# if pdflatex is present, we will generate PDFs from the TeX sources.
+if hash pdflatex 2>/dev/null; then
+    for f in $(ls doc/tex/*.tex); do
+        filename=`basename $f`
+
+        pdflatex --output-directory=./doc/pdf ./doc/tex/$filename
+    done
+
+    rm doc/pdf/*.{aux,log,out}
+else
+    echo "Install pdflatex to convert TeX documentation sources to PDF."
+fi
